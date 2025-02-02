a0167e4f9ae5ebe6aaf2bc5ac50ff156bf85b902
Leandro R. <36233004+ReaperCord@users.noreply.github.com>
2025-02-01 23:59:33 -0300

    Aula5: Estruturas de repetição

---
+++ aulas/aula5/app.ts
@@ -0,0 +1,14 @@
+//for
+//for sintax:
+console.log('----------------------- For --------------------');
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
+//while sintax:
+console.log('----------------------- while --------------------');
+let z: number = 2
+while(z < 6) {
+    console.log(z)
+    z++
+}
