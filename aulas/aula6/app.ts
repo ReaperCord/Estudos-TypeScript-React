5df397945f1ab43bef661dfd718eb35b3b57cb23
Leandro R. <36233004+ReaperCord@users.noreply.github.com>
2025-02-03 18:59:11 -0300

    Aula6: Arrays

---
+++ aulas/aula6/app.ts
@@ -0,0 +1,21 @@
+const array: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+//ou
+//const Array: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(array[0])
+console.log(array[1])
+console.log(array[2])
+console.log(array.length)
+
+const stringArray: Array<string> = ['a', 'b', 'c']
+//ou
+//const stringArray: string[] = ['a', 'b', 'c'];
+console.log(stringArray[0])
+console.log(stringArray[1])
+console.log(stringArray[2])
+console.log(stringArray.length)
+
+/*
+Comando básicos de arrays.
+.push → adiciona um novo elemento a um array
+.pop → remove o último elemento adicionado (Como em uma stack do java)
+*/
